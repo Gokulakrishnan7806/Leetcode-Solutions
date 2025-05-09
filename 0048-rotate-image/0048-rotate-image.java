6f486d6de8eaa6907c6772e527031f5470e95333
Gokula Krishnan <ms.gokul7806@gmail.com>
2025-05-09 20:58:34 +0530

    Time: 12 ms (2.71%), Space: 43.2 MB (8.1%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+
+        
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+
+               int temp=matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+
+                int temp=matrix[i][n-j-1];
+                matrix[i][n-j-1]=matrix[i][j];
+                matrix[i][j]=temp;
+
+            }
+        }
+        print(matrix);
+
+        
+        
+        
+    }
+
+    public void print(int[][] matrix){
+
+        int n=matrix.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
