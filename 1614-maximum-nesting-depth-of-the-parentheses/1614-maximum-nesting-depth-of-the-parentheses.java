b68d069182f725abc602f438067a4cd179bb86c8
Gokula Krishnan <ms.gokul7806@gmail.com>
2025-10-14 12:20:32 +0530

    Time: 0 ms (100%), Space: 42 MB (8.17%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDepth(String str) {
+         int max=0;
+        int openBrackets=0;
+
+        for(char ch : str.toCharArray()){
+            if(ch=='('){
+                openBrackets++;
+            }
+            else if(ch==')'){
+                openBrackets--;
+            }
+           max=Math.max(max,openBrackets);
+        }
+
+        return max;
+        
+    }
+}
